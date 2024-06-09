9d2d12209f10f47975de0461cb9a59b2f6455afb
wajdi-alsalti <68198558+wajdi-alsalti@users.noreply.github.com>
2024-06-09 18:44:31 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,18 +4,31 @@ self.addEventListener("push", (event) => {
         body: notif.body,
         icon: notif.image,
         data: {
-            url: notif.click_action,
-             deeplink: "https://example.co.il/gizmos"
+            // url: notif.click_action,
+             url: "https://example.co.il/gizmos",
+             //deeplink: "https://example.co.il/gizmos"
         }
     }));
 
 });
 
-self.addEventListener("notificationclick", (event) => {
-    const deepLinkUrl = event.notification.data.deeplink;
+// self.addEventListener("notificationclick", (event) => {
+//     const deepLinkUrl = event.notification.data.deeplink;
+//     event.notification.close();
+//     window.location.href = deepLinkUrl;
+// });
+
+// Handle notification click
+self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    window.location.href = deepLinkUrl;
-});
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(function(clientList) {
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+        })
+    );
+    });
 
 // self.addEventListener("notificationclick", (event) => {
 //     event.waitUntil(clients.openWindow(event.notification.data.deeplink));
